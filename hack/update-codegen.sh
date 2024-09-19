e9365781cfa438bde92c05604dace83da587de73
zhouwei9408 <zhouwei9408@gmail.com>
2024-09-19 17:44:01 +0800

    modify

--- hack/update-codegen.sh
+++ hack/update-codegen.sh
@@ -1,22 +1,36 @@
 #!/usr/bin/env bash
 
+# 设置脚本在执行过程中遇到任何错误时立即退出
 set -o errexit
+# 设置脚本在使用未定义的变量时立即退出
 set -o nounset
+# 设置脚本在管道中的任何一个命令失败时立即退出
 set -o pipefail
 
-SCRIPT_ROOT=$(dirname "${BASH_SOURCE[0]}")/..
-CODEGEN_PKG=${SCRIPT_ROOT}/vendor/k8s.io/code-generator
+# generate the code with:
+# --output-base    because this script should also be able to run inside the vendor dir of
+#                  k8s.io/kubernetes. The output-base is needed for the generators to output into the vendor dir
+#                  instead of the $GOPATH directly. For normal projects this can be dropped.
 
-source "${CODEGEN_PKG}/kube_codegen.sh"
 
-THIS_PKG="vcjob-controller"
+$GOPATH/pkg/mod/k8s.io/code-generator@v0.19.2/generate-groups.sh "deepcopy,client,informer,lister" github.com/570540895/vcjob-controller/pkg/client github.com/570540895/vcjob-controller/pkg/apis "batch:v1alpha1" --go-header-file $GOPATH/github.com/570540895/vcjob-controller/hack/boilerplate.go.txt --output-base $GOPATH
 
-kube::codegen::gen_helpers \
-    --boilerplate "${SCRIPT_ROOT}/hack/boilerplate.go.txt" \
-    "${SCRIPT_ROOT}/pkg/apis"
 
-kube::codegen::gen_client \
-    --with-watch \
-    --output-dir "${SCRIPT_ROOT}/pkg/generated" \
-    --output-pkg "${THIS_PKG}/pkg/generated" \
-    "${SCRIPT_ROOT}/pkg/apis"
+
+
+
+
+
+
+
+
+
+$GOPATH/pkg/mod/k8s.io/code-generator@v0.19.2/generate-groups.sh "all" \
+  vcjob-controller/pkg/generated \
+  vcjob-controller/pkg/apis \
+  "batch:v1alpha1" \
+  --go-header-file $(pwd)/boilerplate.go.txt \
+  --output-base $(pwd)/../../
+
+# To use your own boilerplate text append:
+#   --go-header-file "${SCRIPT_ROOT}"/hack/custom-boilerplate.go.txt
